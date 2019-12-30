e3961cae9fc196d2e60a435d3f6853ccd60f9590
Prabaha Gangopadhyay <prabaha.gangopadhyay@yale.edu>
2019-12-29 23:53:13 -0500

    updated patch positions to be on .25 and .75 locations along x and y axis of window

---
+++ +pct/+task/+fixation/+states/fix_hold_patch.m
@@ -0,0 +1,169 @@
+function state = fix_hold_patch(program, conf)
+
+time_in = conf.TIMINGS.time_in;
+
+state = ptb.State();
+state.Name = 'fix_hold_patch';
+
+state.Duration = time_in.(state.Name);
+
+state.Entry = @(state) entry(state, program);
+state.Loop = @(state) loop(state, program);
+state.Exit = @(state) exit(state, program);
+
+end
+
+function entry(state, program)
+
+% Reset fix acquired state and target state.
+state.UserData.fixation_acquired_state = fixation_acquired_state();
+reset( program.Value.targets.fix_square );
+
+reset_targets( program );
+position_stimuli( state, program );
+
+end
+
+function loop(state, program)
+
+draw_target( program );
+draw_patches( program )
+draw_cursor( program );
+flip( program.Value.window );
+
+check_target( state, program );
+
+end
+
+function exit(state, program)
+
+fix_acq_state = state.UserData.fixation_acquired_state;
+
+if ( fix_acq_state.Acquired )
+  next( state, program.Value.states('just_patches') );
+else
+  next( state, program.Value.states('new_trial') );
+end
+
+end
+
+function fix_state = fixation_acquired_state()
+
+fix_state = struct();
+fix_state.Acquired = false;
+fix_state.Entered = false;
+fix_state.Broke = false;
+
+end
+
+function draw_target(program)
+
+is_debug = pct.util.is_debug( program );
+window = program.Value.window;
+fix_square = program.Value.stimuli.fix_square;
+fix_target = program.Value.targets.fix_square;
+
+draw( fix_square, window );
+
+if ( is_debug )
+  draw( fix_target.Bounds, window );
+end
+
+end
+
+function draw_patches( program )
+
+num_patches = count_patches( program );
+stimuli = program.Value.stimuli;
+window = program.Value.window;
+patch_targets = program.Value.patch_targets;
+
+is_debug = pct.util.is_debug( program );
+
+for i = 1:num_patches
+  stimulus = stimuli.(pct.util.nth_patch_stimulus_name(i));
+  draw( stimulus, window );
+  
+  if ( is_debug )
+    draw( patch_targets{i}.Bounds, window );
+  end
+end
+
+end
+
+function draw_cursor(program)
+
+pct.util.draw_gaze_cursor( program );
+
+end
+
+function check_target(state, program)
+
+fix_target = program.Value.targets.fix_square;
+fix_acq_state = state.UserData.fixation_acquired_state;
+
+if ( fix_target.IsDurationMet )
+  % Looked for long enough, so proceed to the exit function.
+  fix_acq_state.Acquired = true;
+  escape( state );
+  
+elseif ( fix_target.IsInBounds )
+  % Mark that we entered the target.
+  fix_acq_state.Entered = true;
+  
+elseif ( fix_acq_state.Entered )
+  % Looked away from the target, so proceed to the exit function.
+  fix_state.Broke = true;
+  escape( state );
+end
+
+state.UserData.fixation_acquired_state = fix_acq_state;
+
+end
+
+function position_stimuli(state, program)
+
+num_patches = count_patches( program );
+stimuli = program.Value.stimuli;
+pos_vec = [.25, .75];
+
+for i = 1:num_patches
+  stim_name = pct.util.nth_patch_stimulus_name( i );
+  new_pos = [pos_vec(randi(length(pos_vec))) pos_vec(randi(length(pos_vec)))];
+  stimulus = stimuli.(stim_name);
+  stimulus.Position.Value = new_pos;
+end
+
+end
+
+function reset_targets(program)
+
+num_patches = count_patches( program );
+stimuli = program.Value.stimuli;
+default_color = default_patch_color( program );
+
+for i = 1:num_patches
+  stim_name = pct.util.nth_patch_stimulus_name( i );
+  stimulus = stimuli.(stim_name);
+  stimulus.FaceColor = default_color;
+end
+
+patch_targets = program.Value.patch_targets;
+
+for i = 1:numel(patch_targets)
+  reset( patch_targets{i} );
+end
+
+end
+
+function num_patches = count_patches(program)
+
+num_patches = program.Value.structure.num_patches;
+
+end
+
+function color = default_patch_color(program)
+
+color = program.Value.stimuli_setup.patch.color;
+
+end
---
+++ +pct/+task/+fixation/+states/just_patches.m
@@ -0,0 +1,126 @@
+function state = just_patches(program, conf)
+
+time_in = conf.TIMINGS.time_in;
+
+state = ptb.State();
+state.Name = 'just_patches';
+
+state.Duration = time_in.(state.Name);
+
+state.Entry = @(state) entry(state, program);
+state.Loop = @(state) loop(state, program);
+state.Exit = @(state) exit(state, program);
+
+end
+
+function entry(state, program)
+
+% All patches remaining
+state.UserData.num_patches_remaining = count_patches( program );
+% No patches were acquired
+state.UserData.patch_elapsed_state = false( 1, count_patches(program) );
+
+reset_targets( program );
+%position_stimuli( state, program );
+
+end
+
+function loop(state, program)
+
+draw_targets( program );
+draw_cursor( program );
+flip( program.Value.window );
+
+check_targets( state, program );
+
+% Exit if all patches were acquired.
+if ( state.UserData.num_patches_remaining == 0 )
+  escape( state );
+end
+
+end
+
+function exit(state, program)
+
+next( state, program.Value.states('new_trial') );
+
+end
+
+function draw_targets(program)
+
+num_patches = count_patches( program );
+stimuli = program.Value.stimuli;
+window = program.Value.window;
+patch_targets = program.Value.patch_targets;
+
+is_debug = pct.util.is_debug( program );
+
+for i = 1:num_patches
+  stimulus = stimuli.(pct.util.nth_patch_stimulus_name(i));
+  draw( stimulus, window );
+  
+  if ( is_debug )
+    draw( patch_targets{i}.Bounds, window );
+  end
+end
+
+end
+
+function draw_cursor(program)
+
+pct.util.draw_gaze_cursor( program );
+
+end
+
+function check_targets(state, program)
+
+patch_targets = program.Value.patch_targets;
+stimuli = program.Value.stimuli;
+
+for i = 1:numel(patch_targets)
+  stim_name = pct.util.nth_patch_stimulus_name( i );
+  stimulus = stimuli.(stim_name);
+    
+  if ( patch_targets{i}.IsDurationMet )
+    stimulus.FaceColor = [0, 0, 0];
+    
+    if ( ~state.UserData.patch_elapsed_state(i) )
+      state.UserData.patch_elapsed_state(i) = true;
+      state.UserData.num_patches_remaining = state.UserData.num_patches_remaining - 1;
+    end
+  end
+end
+
+end
+
+function reset_targets(program)
+
+num_patches = count_patches( program );
+stimuli = program.Value.stimuli;
+default_color = default_patch_color( program );
+
+for i = 1:num_patches
+  stim_name = pct.util.nth_patch_stimulus_name( i );
+  stimulus = stimuli.(stim_name);
+  stimulus.FaceColor = default_color;
+end
+
+patch_targets = program.Value.patch_targets;
+
+for i = 1:numel(patch_targets)
+  reset( patch_targets{i} );
+end
+
+end
+
+function num_patches = count_patches(program)
+
+num_patches = program.Value.structure.num_patches;
+
+end
+
+function color = default_patch_color(program)
+
+color = program.Value.stimuli_setup.patch.color;
+
+end
