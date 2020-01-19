7c1602648a0b070350b257f8c827116dbdc93a34
Prabaha Gangopadhyay <prabaha.gangopadhyay@yale.edu>
2020-01-18 19:28:43 -0500

    Added new patch definitions to config file

--- script/pct_start_task.m
+++ script/pct_start_task.m
@@ -4,8 +4,15 @@ conf.TIMINGS.time_in.fixation = 10;
 
 conf.META.subject = '';
 
-conf.INTERFACE.use_mouse = false;
-conf.INTERFACE.use_reward = true;
+conf.INTERFACE.use_mouse = true;
+conf.INTERFACE.use_reward = false;
 conf.INTERFACE.skip_sync_tests = true;
+
+
+conf.DEBUG_SCREEN.is_present = true;
+conf.DEBUG_SCREEN.index = 0;
+conf.DEBUG_SCREEN.background_color = [ 0 0 0 ];
+conf.DEBUG_SCREEN.rect = [ 600, 600, 1000, 1000 ];
+
 
 pct.task.fixation.start( conf );
