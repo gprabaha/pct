f08edec45bbce52f9abab8f796bd586583d4275e
setupTwoGitUser <setuptwoemail@gmail.com>
2020-02-24 11:46:44 -0500

    updated the fixation stage to start training at

--- +pct/+training/+configure/fixation_training.m
+++ +pct/+training/+configure/fixation_training.m
@@ -3,7 +3,7 @@ function fixation_training(manager)
 %add_stage( manager, pct.training.stages.Fixation1() );
 %add_stage( manager, pct.training.stages.Fixation2() );
 %add_stage( manager, pct.training.stages.Fixation3() );
-add_stage( manager, pct.training.stages.Fixation4() );
+%add_stage( manager, pct.training.stages.Fixation4() );
 add_stage( manager, pct.training.stages.Fixation5() );
 add_stage( manager, pct.training.stages.FixHold1() );
 add_stage( manager, pct.training.stages.FixHold2() );
