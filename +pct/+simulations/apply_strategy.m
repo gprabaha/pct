c75b0e580b87d64dfc9122f1b53cbfa39a8e5297
gprabaha <prabaha.gangopadhyay@yale.edu>
2020-04-30 14:58:39 -0400

    implemented 'selfish' strategy in apply_strategy

--- +pct/+simulations/apply_strategy.m
+++ +pct/+simulations/apply_strategy.m
@@ -1,6 +1,22 @@
-function patch_choice = apply_strategy(monkey_identity, strategy, error_rate, ...
-  patches, acquired_patches)
+function patch_choice = apply_strategy(monkey_identity, strategy, ...
+  choice_prob_list, error_rate, patches, acquired_patches)
 
-disp('Hi');
+available_patch_indices = acquired_patches==0;
+self_patch = monkey_identity;
+competition = 3;
+cooperation = 4;
+
+switch strategy
+  
+  case 'selfish'
+    preference_order = [self_patch competition cooperation];
+    available_choices = unique( patches(available_patch_indices) );
+    if any(ismember(available_choices, preference_order))
+      [~, choice_indices] = ismember(preference_order, available_choices);
+      choice_indices = choice_indices(choice_indices>0);
+      probabilities = choice_prob_list(choice_indices);
+      probabilities = probabilities/sum(probabilities);
+      patch_choice = randsample(preference_order, 1, true, probabilities);
+    end
     
 end
--- +pct/+simulations/main.m
+++ +pct/+simulations/main.m
@@ -9,6 +9,7 @@ m2_error_rate = 0.1; % Error rate per saccade for m2
 
 m1_win_prob = 0.5; % Probability of m1 willing in competition
 coop_reward = 0.5; % Fraction of reward each monkey gets for acquiring a cooperation patch
+choice_prob_list = [0.6 0.25 0.15]; % Hierarchical choice probability; preferred -> unpreferred
 
 n_reps = 10; % Number of repetitions for each trial type
 n_sessions = 20; % Number of experimental sessions
@@ -24,8 +25,8 @@ for m1_strategy = strategy_list
   for m2_strategy = strategy_list
     for session = 1:n_sessions
       pct.simulations.pit_strategies( m1_strategy, m2_strategy,...
-        m1_error_rate, m2_error_rate, m1_win_prob, coop_reward, max_moves, ...
-        n_reps, num_patches, save_data_flag, save_fig_flag );
+        m1_error_rate, m2_error_rate, choice_prob_list, m1_win_prob, ...
+        coop_reward, max_moves, n_reps, num_patches, save_data_flag, save_fig_flag );
     end
   end
 end
