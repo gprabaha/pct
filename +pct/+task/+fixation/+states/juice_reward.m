c9e395e2d749c29d661f3ce3c1b6701328364159
gprabaha <prabaha.gangopadhyay@yale.edu>
2020-07-20 12:23:14 -0400

    updated reward delivery for multiple patches and during pause state

--- +pct/+task/+fixation/+states/juice_reward.m
+++ +pct/+task/+fixation/+states/juice_reward.m
@@ -17,15 +17,32 @@ function entry(state, program)
 
 flip( program.Value.window );
 debug_window_is_present = program.Value.debug_window_is_present;
+
 if (debug_window_is_present)
   flip( program.Value.debug_window );
 end
-WaitSecs(0.2)
-give_juice_reward( program );
+
+state.UserData.reward_timer = nan;
+state.UserData.num_pulses = 0;
 
 end
 
 function loop(state, program)
+
+quantity = program.Value.rewards.training;
+inter_pulse_interval = 1e-2;  % 10ms;
+num_collected_patches = sum( ~isnan( program.Value.data.Value(end).just_patches.patch_acquired_times ) );
+reward_timer = state.UserData.reward_timer;
+pulse_duration = quantity;
+
+if ( state.UserData.num_pulses < num_collected_patches )
+  if ( isnan(reward_timer) || ...
+       toc(reward_timer) > pulse_duration + inter_pulse_interval )
+    pct.util.deliver_reward( program, 1, quantity );
+    state.UserData.reward_timer = tic();
+    state.UserData.num_pulses = state.UserData.num_pulses + 1;
+  end
+end
 
 end
 
@@ -33,15 +50,5 @@ function exit(state, program)
 
 states = program.Value.states;
 next( state, states('new_trial') );
-
-
-end
-
-function give_juice_reward(program)
-
-quantity = program.Value.rewards.training;
-% Check this with Nick
-collected = sum( ~isnan( program.Value.data.Value(end).just_patches.patch_acquired_times ) );
-pct.util.deliver_reward( program, 1, collected*quantity );
 
 end
